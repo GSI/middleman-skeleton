45e5d98beeac99b9cb103e7e97704f4c50ebf0e2
GSI2015 <2015@groovy-skills.com>
2017-06-12 11:44:30 -0500

    removed Foundation framework

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,9 +1,6 @@
 //= require vendor/modernizr.js
 //= require vendor/jquery.js
 /*
-= require foundation/foundation
-require foundation/foundation.clearing
-require foundation/foundation.orbit
 = require piwik
 */
 //= require_directory
